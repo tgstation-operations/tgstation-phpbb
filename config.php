395aae339bba1426eaa738a16a52b5a27fb12277
Nick <nick@nfreader.net>
2025-02-14 19:22:17 -0500

    Grab database credentials from ENV

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 // phpBB 3.3.x auto-generated configuration file
 // Do not change anything in this file!
 $dbms = 'phpbb\\db\\driver\\mysqli';
-$dbhost = 'db';
-$dbport = '';
-$dbname = 'db';
-$dbuser = 'db';
-$dbpasswd = 'db';
+$dbhost = $_ENV['DB_HOST'];
+$dbport = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_NAME'];
+$dbuser = $_ENV['DB_USER'];
+$dbpasswd = $_ENV['DB_PASSWORD'];
 $table_prefix = '';
 $phpbb_adm_relative_path = 'adm/';
 $acm_type = 'phpbb\\cache\\driver\\file';
